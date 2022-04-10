bd36fac85b9a0e5a413616e7a1b0248938a125c5
Ekaterina Slotina <ek.marenova@yandex.ru>
2022-04-10 22:23:37 +0200

    Added static root

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -120,5 +120,6 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
