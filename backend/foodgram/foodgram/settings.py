1d29610b3dd6f39e5a570a886bd9b151a21ed2a4
Ekaterina Slotina <ek.marenova@yandex.ru>
2022-04-21 19:44:01 +0200

    debug fixes

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['backend', '51.250.31.237', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['http://51.250.31.237', 'http://127.0.0.1', 'http://backend']
